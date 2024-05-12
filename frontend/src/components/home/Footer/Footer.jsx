cfda132ecdbd6d35b1a125f0af5f7a51a85cb5f4
Roberto Frank Estrada Arias <rfestradarias@gmail.com>
2024-05-13 00:32:08 +0200

    Last Changes! arreglando las carpetas a PascalCase

---
+++ frontend/src/components/home/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from './Footer.module.css';
+
+const Footer = () => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.footermenu}>
+            <Link to="/novedades">
+                <button className={styles.footermain}>NOVEDADES</button>
+            </Link>
+            <Link to="/precios">
+                <button className={styles.footermain}>PRECIOS</button>
+            </Link>
+            <Link to="/pages">
+                <button className={styles.footermain}>AYUDA</button>
+            </Link>
+            <Link to="/terminos">
+                <button className={styles.footerlegal}>TÉRMINOS</button>
+            </Link>
+            <Link to="/privacidad">
+                <button className={styles.footerlegal}>PRIVACIDAD</button>
+            </Link>
+            <Link to="/seguridad">
+                <button className={styles.footerlegal}>SEGURIDAD</button>
+            </Link>
+        </div>
+      </div>
+    );
+  };
+  
+export default Footer;
