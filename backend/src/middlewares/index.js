29273749d6708c52ecaa810c53359836b77b09c5
Roberto Frank Estrada Arias <rfestradarias@gmail.com>
2024-05-13 00:10:39 +0200

    Last Change!

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -3,47 +3,46 @@ const validateEmail = (email) => {
   return pattern.test(email);
 };
 
-
 const validateDate = (eventDate) => {
-    console.log(eventDate, "Event Date")
-    const pattern = /^\d{4}-\d{2}-\d{2}$/;
-    return pattern.test(eventDate);
-  };
-  
-  const validateTitle = (eventTitle) => {
-    return eventTitle.trim() !== '';
-  };
-  
+  console.log(eventDate, 'Event Date');
+  const pattern = /^\d{4}-\d{2}-\d{2}$/;
+  return pattern.test(eventDate);
+};
+
+const validateTitle = (eventTitle) => {
+  return eventTitle.trim() !== '';
+};
+
 const validatePrice = (eventPrice) => {
-    console.log("Event Price", eventPrice)
-    return !isNaN(eventPrice) && parseFloat(eventPrice) >= 0;
+  console.log('Event Price', eventPrice);
+  return !isNaN(eventPrice) && parseFloat(eventPrice) >= 0;
 };
-  
+
 const validateCapacity = (eventCapacity) => {
-    console.log("Event Capacity", eventCapacity)
-    if (eventCapacity === "ilimitado") {
-        return -1; 
-    } else {
-        return !isNaN(eventCapacity) && parseInt(eventCapacity) >= 0;
-    }
+  console.log('Event Capacity', eventCapacity);
+  if (eventCapacity === 'ilimitado') {
+    return -1;
+  } else {
+    return !isNaN(eventCapacity) && parseInt(eventCapacity) >= 0;
+  }
 };
-  
+
 const validateEventCreation = (req, res, next) => {
   console.log(req.body);
   const { eventDate, eventTitle, eventPrice, eventCapacity } = req.body;
-  console.log(typeof(eventPrice))
-  console.log(eventCapacity)
-  
-    if (!eventDate || !validateDate(eventDate)) {
-      return res.status(400).json({ error: 'La fecha del evento es requerida' });
-    }
-    if (!eventTitle || !validateTitle(eventTitle)) {
-      return res.status(400).json({ error: 'El título del evento es requerido' });
-    }
-    // if (!eventPrice || !validatePrice(eventPrice)) {
-    //   return res.status(400).json({ error: 'Debe establecerse un precio válido para la entrada' });
+  console.log(typeof eventPrice);
+  console.log(eventCapacity);
+
+  if (!eventDate || !validateDate(eventDate)) {
+    return res.status(400).json({ error: 'La fecha del evento es requerida' });
+  }
+  if (!eventTitle || !validateTitle(eventTitle)) {
+    return res.status(400).json({ error: 'El título del evento es requerido' });
+  }
+  // if (!eventPrice || !validatePrice(eventPrice)) {
+  //   return res.status(400).json({ error: 'Debe establecerse un precio válido para la entrada' });
   // }
-  
+
   if (eventPrice === undefined || eventPrice === null) {
     return res.status(400).json({ error: 'El precio del evento es requerido' });
   } else {
@@ -51,18 +50,16 @@ const validateEventCreation = (req, res, next) => {
       return res.status(400).json({ error: 'El precio del evento no es válido' });
     }
   }
-  
-    if (eventCapacity === undefined || eventCapacity === null) {
-        return res.status(400).json({ error: 'La capacidad del evento es requerida' });
-    }
-    if (!validateCapacity(eventCapacity)) {
-        return res.status(400).json({ error: 'La capacidad del evento no es válida' });
-    }
-  
-    next();
-  };
 
+  if (eventCapacity === undefined || eventCapacity === null) {
+    return res.status(400).json({ error: 'La capacidad del evento es requerida' });
+  }
+  if (!validateCapacity(eventCapacity)) {
+    return res.status(400).json({ error: 'La capacidad del evento no es válida' });
+  }
 
+  next();
+};
 
 const validatePassword = (password) => {
   const pattern = new RegExp(/^.{5,}$/);
@@ -85,33 +82,32 @@ const validateUserCreation = (req, res, next) => {
   if (!fullname) {
     return res.status(400).json({ error: 'Fullname is required' });
   }
-  if (!email) {
-    return res.status(400).json({ error: 'Email is required' });
-  }
-  if (!phone_number) {
-    return res.status(400).json({ error: 'Phone number is required' });
-  }
-  if (!password) {
-    return res.status(400).json({ error: 'Password is required' });
-  }
-  if (!birthdate) {
-    return res.status(400).json({ error: 'Birthdate is required' });
-  }
-
-  const formattedBirthdate = formatDate(birthdate);
-
   if (!validateFullname(fullname)) {
     return res.status(400).json({ error: 'Invalid fullname format' });
+  }
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' });
   }
   if (!validateEmail(email)) {
     return res.status(400).json({ error: 'Invalid email format' });
   }
+  if (!birthdate) {
+    return res.status(400).json({ error: 'Birthdate is required' });
+  }
+  if (!phone_number) {
+    return res.status(400).json({ error: 'Phone number is required' });
+  }
   if (!validateSpanishPhoneNumber(phone_number)) {
     return res.status(400).json({ error: 'Invalid spanish phone number format' });
+  }
+  if (!password) {
+    return res.status(400).json({ error: 'Password is required' });
   }
   if (!validatePassword(password)) {
     return res.status(400).json({ error: 'Password must be greater than 4 characters' });
   }
+
+  const formattedBirthdate = formatDate(birthdate);
 
   req.body.birthdate = formattedBirthdate;
 
@@ -126,9 +122,7 @@ const formatDate = (dateString) => {
   return `${year}-${month}-${day}`;
 };
 
-  
 module.exports = {
   validateEventCreation,
   validateUserCreation,
-
 };
