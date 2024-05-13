3f04fec7752021561c54f2339bd6fd2eb1371ba5
DIEGUCHIN\ruizd <ruizdiego.-@hotmail.com>
2024-05-13 09:48:04 +0200

    Co-authored-by: Arian <Arian1192@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { Routes, Route } from 'react-router-dom';
 import UserLogin from './components/UserLogin/UserLogin';
 import UserCreate from './components/UserCreate/UserCreate';
-import EventFormContainer from "./components/EventFormContainer/EventFormContainer";
-import Styles from './App.module.css'
+import Home from './components/home/HomeContainer/Home';
+import UserList from './components/users/UserList/UserList';
+import UserDetail from './components/users/UserDetail/UserDetail';
+import UserLoginCreate from './components/users/UserLoginCreate/UserLoginCreate';
+import NavBar from './components/home/NavBar/NavBar';
+import Footer from './components/home/Footer/Footer';
+import Pages from './components/Pages/Pages';
+import EventFormContainer from './components/eventFormContainer/EventFormContainer';
+import Styles from './App.module.css';
+
 const queryClient = new QueryClient();
 
 function App() {
