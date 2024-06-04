313672d747ea3b324d73d7100ec3768a4be49dda
Roberto Frank Estrada Arias <rfestradarias@gmail.com>
2024-06-04 20:58:37 +0200

    Test

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', mainRouter);
+app.use('/', mainRouter);
 
 connectDB().then(() => console.log('Connected to database!'));
 
