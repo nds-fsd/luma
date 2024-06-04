c729d683d4ba435fe412a3a8f31d8b4a44ba909a
Roberto Frank Estrada Arias <rfestradarias@gmail.com>
2024-06-04 23:33:51 +0200

    Test!

--- frontend/src/components/users/UserLoginCreate/UserLoginCreate.jsx
+++ frontend/src/components/users/UserLoginCreate/UserLoginCreate.jsx
@@ -1,18 +1,9 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
 import UserLogin from './UserLogin/UserLogin';
 
 const UserLoginCreate = ({ handleLogin }) => {
-  const { handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-    // Aquí puedes agregar lógica para manejar el inicio de sesión
-  };
-
   return (
     <div>
-      <UserLogin onSubmit={handleSubmit(onSubmit)} handleLogin={handleLogin}/>
+      <UserLogin handleLogin={handleLogin} />
     </div>
   );
 };
