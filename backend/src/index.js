59280f776eb746d59ecf3714fbfc2bbb2c3490c4
Roberto Frank Estrada Arias <rfestradarias@gmail.com>
2024-06-04 22:04:23 +0200

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: ['https://lumatic.netlify.app'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/', mainRouter);
