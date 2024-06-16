169c456ae4867ec74d68f30f680a0925e8aa4c2f
Roberto Frank Estrada Arias <rfestradarias@gmail.com>
2024-06-16 18:38:53 +0200

    Retoques pendientes!

--- frontend/src/components/events/eventList/EventList.jsx
+++ frontend/src/components/events/eventList/EventList.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useQuery, useQueryClient } from 'react-query';
 import { Link, useNavigate } from 'react-router-dom';
 import styles from './EventList.module.css';
