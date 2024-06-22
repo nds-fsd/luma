807e2cc4b5cc8202bdb994259c5c3bca3c1b89f6
Roberto Frank Estrada Arias <rfestradarias@gmail.com>
2024-06-22 18:02:49 +0200

    Test

--- backend/src/bootstrap.js
+++ backend/src/bootstrap.js
@@ -1,19 +1,16 @@
 const cors = require('cors');
 const express = require('express');
-const mainRouter = require('./routers/MainRouter');
+const mainRouter = require('./routers/index');
+
 
 exports.bootstrapApp = () => {
   const app = express();
-
-  // Configurar CORS para permitir solicitudes desde Netlify y localhost
-  const corsOptions = {
-    origin: ['https://lumatic.netlify.app', 'http://localhost:3000'], // Agrega aquí tu dominio de Netlify y localhost para desarrollo
-    optionsSuccessStatus: 200,
-  };
-
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(express.json());
-  app.use('/', mainRouter); // Sin prefijo '/api'
+  app.use('/', mainRouter);
 
+  
   return app;
-};
+}
+
+
