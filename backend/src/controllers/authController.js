a74bc77c9a40f2f5e76f8a5f78403a4b444ffbb9
Barbara Salazar <157834420+barbarasg92@users.noreply.github.com>
2024-07-05 19:32:07 +0200

    actualizacion emails

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,53 @@
+const { User } = require('../schemas/user.schema');
+const bcrypt = require('bcryptjs');
+const { generateJWT } = require('../services/auth.service');
+const { sendWelcomeEmail } = require('../services/email.service');
+
+exports.register = async (req, res) => {
+
+
+    const { name, email, password } = req.body
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        return res.status(401).json({ message: "User already registered" })
+    }
+
+    const encryptedPassword = bcrypt.hashSync(password);
+
+    const newUser = await User.create({ email, name, password: encryptedPassword, active: false });
+
+
+    const token = generateJWT(newUser);
+
+    await sendWelcomeEmail(email, name)
+
+    return res.status(201).json({ user: newUser, token });
+}
+
+
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body
+
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+        return res.status(401).json({ message: "User not registered" })
+    }
+
+
+    const passwordsMatch = await bcrypt.compare(password, existingUser.password);
+
+
+    if (!passwordsMatch) {
+        return res.status(401).json({ message: "Passwords do not match" })
+
+    }
+
+    const token = generateJWT(existingUser);
+
+    return res.status(200).json({ user: existingUser, token });
+}
